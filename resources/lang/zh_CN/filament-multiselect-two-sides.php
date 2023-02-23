868a5ec0566929272f7adfb85122489c3ff5bef0
Cooper <myxiaoao@gmail.com>
2023-02-23 19:34:13 +0800

    add zh_CN translation

---
+++ resources/lang/zh_CN/filament-multiselect-two-sides.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'selectable' => [
+        'label' => '可选择',
+        'placeholder' => '搜索',
+    ],
+
+    'selected' => [
+        'label' => '已选择',
+        'placeholder' => '搜索',
+    ],
+
+];
