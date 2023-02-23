8c363b9bb8c70a9b6013f55a87f5ab7cedd5730f
Léo França <leoarkiteto@gmail.com>
2023-02-23 18:23:00 +0000

    :memo: Fix bug dark mode

--- resources/views/forms/components/fields/multiselect-two-sides.blade.php
+++ resources/views/forms/components/fields/multiselect-two-sides.blade.php
@@ -10,9 +10,10 @@
     :hint-icon="$getHintIcon()"
     :required="$isRequired()"
     :state-path="$getStatePath()"
+
 >
     <div class="flex w-full transition duration-75 text-sm"
-    x-data="{
+        x-data="{
         state: $wire.{{ $applyStateBindingModifiers('entangle(\'' . $getStatePath() . '\')') }},
         options: @js($getOptionsForJs()),
         selectedOptions: [],
@@ -83,28 +84,47 @@
             }
         }
     }"
-    x-init="init"
+        x-init="init"
     >
 
         {{-- Selectable Options --}}
-        <div class="flex-1">
-            <p class="bg-gray-300 text-center w-full py-2 rounded-t-lg dark:bg-gray-600">{{$getSelectableLabel()}}</p>
-            <div class="p-2 border border-gray-300 bg-white rounded-b-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
+        <div class="flex-1 border overflow-hidden rounded-lg shadow-sm"
+            :class="{
+            'bg-white border-gray-300': !@js(config('filament.dark_mode')),
+            'dark:bg-gray-700 dark:border-gray-600': @js(config('filament.dark_mode'))
+            }"
+        >
+            {{-- Title --}}
+            <p class='text-center w-full py-4'
+                :class="{
+                'bg-gray-300': !@js(config('filament.dark_mode')),
+                'dark:bg-gray-600': @js(config('filament.dark_mode'))
+                }"
+            >
+                {{$getSelectableLabel()}}
+            </p>
+            <div class="p-2">
+                {{-- Search Input --}}
                 @if($isSearchable())
                     <input
-                            id="ms_input-search-selectable"
-                            placeholder="{{__('filament-multiselect-two-sides::filament-multiselect-two-sides.selectable.placeholder')}}"
-                            class="w-full border-gray-300 border py-2 px-1 mb-2 rounded"
-                            @keyup="searchAvailableOptions($event.target.value)"
+                        id="ms_input-search-selectable"
+                        placeholder="{{__('filament-multiselect-two-sides::filament-multiselect-two-sides.selectable.placeholder')}}"
+                        class="w-full border-gray-300 border py-2 px-1 mb-2
+                        rounded focus:outline-none focus:ring-2
+                        focus:ring-primary-500"
+                        :class="{
+                            'bg-gray-100': !@js(config('filament.dark_mode')),
+                             'dark:bg-gray-600 dark:border-gray-500': @js(config('filament.dark_mode'))
+                        }"
+                        @keyup="searchAvailableOptions($event.target.value)"
                     />
                 @endif
+                {{-- Option List --}}
                 <ul class="h-48 overflow-y-auto">
                     <template x-for="option in availableOptions.filter(opt=>!state.includes(opt.value))">
-                        <li
-                            class="cursor-pointer p-1 hover:bg-primary-500 hover:text-white transition"
+                        <li class="cursor-pointer p-1 hover:bg-primary-500 hover:text-white transition"
                             x-text="option.label"
-                            @click="selectOption(option.value)"
-                        />
+                            @click="selectOption(option.value)"></li>
                     </template>
                 </ul>
             </div>
@@ -113,35 +133,58 @@
         {{-- Arrow Actions --}}
         <div class="justify-center flex flex-col px-2 space-y-2 translate-y-4">
             <p
-               @click="selectAll"
-               class="cursor-pointer p-1 hover:bg-primary-500 group"
+                @click="selectAll"
+                class="cursor-pointer p-1 hover:bg-primary-500 group"
             >
                 <x-heroicon-o-chevron-double-right class="w-5 h-5 text-primary-500 group-hover:text-white" />
             </p>
-            <p @click="unselectAll" class="cursor-pointer p-1 hover:bg-primary-500 group">
+            <p @click="unselectAll"
+                class="cursor-pointer p-1 hover:bg-primary-500 group">
                 <x-heroicon-o-chevron-double-left class="w-5 h-5 text-primary-500 group-hover:text-white" />
             </p>
         </div>
 
         {{-- Selected Options --}}
-        <div class="flex-1">
-            <p class="bg-gray-300 text-center w-full py-2 rounded-t-lg dark:bg-gray-600">{{$getSelectedLabel()}}</p>
-            <div class="p-2 border border-gray-300 bg-white rounded-b-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
+        <div class="flex-1 border overflow-hidden rounded-lg shadow-sm"
+            :class="{
+                    'bg-white border-gray-300': ! (@js($getStatePath()) in $wire.__instance.serverMemo.errors),
+                    'dark:bg-gray-700 dark:border-gray-600': ! (@js($getStatePath()) in $wire.__instance.serverMemo.errors) && @js(config('filament.dark_mode')),
+                    'bg-white border-danger-600': (@js($getStatePath()) in $wire.__instance.serverMemo.errors),
+                    'dark:bg-gray-700 dark:border-danger-400': (@js($getStatePath()) in $wire.__instance.serverMemo.errors) && @js(config('filament.dark_mode')),
+                }"
+        >
+            {{-- Title --}}
+            <p class='text-center w-full py-4 rounded-t-lg'
+                :class="{
+                'bg-gray-300': !@js(config('filament.dark_mode')),
+                'dark:bg-gray-600': @js(config('filament.dark_mode'))
+                }"
+            >{{$getSelectedLabel()}}</p>
+            <div class="p-2"
+            >
+                {{-- Search Input --}}
                 @if($isSearchable())
                     <input
-                            id="ms_input-search-selected"
-                            placeholder="{{__('filament-multiselect-two-sides::filament-multiselect-two-sides.selected.placeholder')}}"
-                            class="w-full border-gray-300 border py-2 px-1 mb-2 rounded"
-                            @keyup="searchSelectedOptions($event.target.value)"
+                        id="ms_input-search-selectable"
+                        placeholder="{{__('filament-multiselect-two-sides::filament-multiselect-two-sides.selectable.placeholder')}}"
+                        class="w-full border-gray-300 border py-2 px-1 mb-2
+                        rounded focus:outline-none focus:ring-2
+                        focus:ring-primary-500"
+                        :class="{
+                            'bg-gray-100': !@js(config('filament.dark_mode')),
+                            'dark:bg-gray-600 dark:border-gray-500': @js(config('filament.dark_mode'))
+                        }"
+                        @keyup="searchSelectedOptions($event.target.value)"
                     />
                 @endif
+                {{--  Options List --}}
                 <ul class="h-48 overflow-y-auto">
                     <template x-for="option in selectedOptions">
                         <li
                             class="cursor-pointer p-1 hover:bg-primary-500 hover:text-white transition"
                             x-text="options.find(opt=>opt.value === option).label"
                             @click="unselectOption(option)"
-                        />
+                        ></li>
                     </template>
                 </ul>
             </div>
